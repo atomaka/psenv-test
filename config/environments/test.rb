acbd5d1462d94fa5b91388ec2009c9e93a5cf2e7
Andrew Tomaka <atomaka@gmail.com>
2019-10-03 22:48:18 -0400

    Remove remaining non-essential pieces of Rails

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -21,14 +21,14 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  # config.action_controller.perform_caching = false
   config.cache_store = :null_store
 
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
 
   # Disable request forgery protection in test environment.
-  config.action_controller.allow_forgery_protection = false
+  # config.action_controller.allow_forgery_protection = false
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
